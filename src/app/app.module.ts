789195ad0ebbf63343a423480330257f25572df4
Francis <francis@serverless-secure.com-at-025181935248>
2024-07-05 09:57:11 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BiddingComponent } from './bidding/bidding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BiddingComponent
   ],
   imports: [
     BrowserModule
